06825a48072b7cdb4c076417947124e81b7cb69b
Daniel <danielpardinho321@gmail.com>
2023-09-28 22:09:24 -0300

    get movies route

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const server = express();
+const movies = require("./src/data/movies.json");
 
 server.get("/", (req, res) => {
   return res.json({ message: "API is working" });
 });
 
+server.get("/movies", (req, res) => {
+  return res.json(movies);
+});
+
 server.listen(3000, () => {
-  console.log("Servidor está funcionando...");
+  console.log("Server is working...");
 });
